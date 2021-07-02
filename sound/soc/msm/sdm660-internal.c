22da3d9330b4a6c10f982273b15765874c354d8e
LuK1337 <priv.luk@gmail.com>
2021-07-02 11:14:05 +0200

    ASoC: sdm660: Import headset button threshold changes from 50.2.A.0.400
    Change-Id: If79186ad210acfefcee688547c104c94b67139c5

--- sound/soc/msm/sdm660-internal.c
+++ sound/soc/msm/sdm660-internal.c
@@ -1334,12 +1334,22 @@ static void *def_msm_int_wcd_mbhc_cal(void)
 	 */
 	btn_low[0] = 75;
 	btn_high[0] = 75;
-	btn_low[1] = 150;
-	btn_high[1] = 150;
+	btn_low[1] = 110;
+	btn_high[1] = 110;
 	btn_low[2] = 225;
 	btn_high[2] = 225;
+#if defined(CONFIG_MACH_SONY_PIONEER) || \
+	defined(CONFIG_MACH_SONY_PIONEER_DSDS) || \
+	defined(CONFIG_MACH_SONY_DISCOVERY) || \
+	defined(CONFIG_MACH_SONY_DISCOVERY_DSDS) || \
+	defined(CONFIG_MACH_SONY_VOYAGER) || \
+	defined(CONFIG_MACH_SONY_VOYAGER_DSDS)
+	btn_low[3] = 435;
+	btn_high[3] = 435;
+#else
 	btn_low[3] = 450;
 	btn_high[3] = 450;
+#endif
 	btn_low[4] = 500;
 	btn_high[4] = 500;
 
